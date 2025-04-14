9714a4e3e89bae4d7912a07db7fc22db257bf508
Anmol Sharma <anmol0602sharma@gmail.com>
2025-04-14 07:26:13 +0530

    bug fix on logout

--- web/resume/src/app/login/page.js
+++ web/resume/src/app/login/page.js
@@ -1,15 +1,31 @@
 "use client";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 import AuthForm from "@/components/authForm/authForm";
 import { login } from "@/services/authService";
 
 export default function LoginPage() {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const [hasRefreshed, setHasRefreshed] = useState(false);
+
+  useEffect(() => {
+    const refreshed = searchParams.get("refreshed");
+
+    // Wait until component has mounted
+    if (!refreshed && !hasRefreshed) {
+      setHasRefreshed(true);
+      // Let the page fully render, then refresh
+      setTimeout(() => {
+        router.replace("/login?refreshed=true");
+      }, 0); // 0ms lets the UI load before redirect
+    }
+  }, [router, searchParams, hasRefreshed]);
 
   const handleLogin = async (email, password) => {
     try {
       await login(email, password);
-      router.push("/dashboard"); // Redirect after login
+      router.push("/dashboard");
     } catch (error) {
       console.log(error);
     }
